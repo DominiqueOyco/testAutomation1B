d2fba73352caf6d4941781eb6166c931ef997357
Dominique Oyco <dominickoyco@gmail.com>
2025-05-16 15:53:13 -0700

    Added rolls json file

--- test/tests/rolls.spec.ts
+++ test/tests/rolls.spec.ts
@@ -1,1 +1,6 @@
 import { test, expect } from '@playwright/test';
+import data from '../../test-data/rolls.json'
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('https://www.avery.com/custom-printing/labels/calculator/rolls');
+});
